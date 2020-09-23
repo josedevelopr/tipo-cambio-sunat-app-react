57511b72fc1f886c2ada0b9cbe674075f1b659f4
jose antonio alvino velasque <jose_antonio_alvino21@outlook.es>
2020-09-23 12:47:55 -0500

    Store, index reducers, errorReducer and types added

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
-
-import './App.css';
 import CalculadoraTipoCambio from './components/CalculadoraTipoCambio/CalculadoraTipoCambio';
 import CalendarButton from './components/Home/CalendarButton';
 import Home from './components/Home/Home';
 import TipoCambioMensual from './components/TipoCambioMensual/TipoCambioMensual';
+import {Provider} from "react-redux";
+import store from "./store";
+import './App.css';
+
 
 function App() {
   return (
-    <Router>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/tipo-cambio-por-mes" component={TipoCambioMensual} />      
-      <Route exact path="/calculo-tipo-cambio" component={CalculadoraTipoCambio} />      
-    </Router>
-      
+    <Provider store={store}>
+      <Router>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/tipo-cambio-por-mes" component={TipoCambioMensual} />      
+        <Route exact path="/calculo-tipo-cambio" component={CalculadoraTipoCambio} />      
+      </Router>
+    </Provider>
   );
 }
 
